b935d1ed7d7d9a46b12b2d870e0d8306b7c6f1bf
Frankie <email@frankiescott.net>
2019-06-11 12:23:56 -0400

    added a health bar display

--- core/src/edu/binghamton/cs/Animation.java
+++ core/src/edu/binghamton/cs/Animation.java
@@ -2,9 +2,11 @@ package edu.binghamton.cs;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.InputListener;
 import com.badlogic.gdx.scenes.scene2d.Stage;
@@ -27,6 +29,7 @@ public class Animation extends ApplicationAdapter {
 	float w, h;
 	int imgWidth, imgHeight;
 	float gravity;
+	ShapeRenderer shapeRenderer;
 
 	private Skin loadSkin() {
 		return new Skin(Gdx.files.internal("clean-crispy-ui.json"));
@@ -37,6 +40,7 @@ public class Animation extends ApplicationAdapter {
 		batch = new SpriteBatch();
 		stage = new Stage(new ScreenViewport());
 		Gdx.input.setInputProcessor(stage);
+		shapeRenderer = new ShapeRenderer();
 
 		img = new Texture("badlogic.jpg");
 		imgWidth = img.getWidth();
@@ -46,7 +50,6 @@ public class Animation extends ApplicationAdapter {
 		dx = 15;
 		dy = 0;
 		gravity = -4;
-
 		configureControlInterface();
 	}
 
@@ -119,7 +122,7 @@ public class Animation extends ApplicationAdapter {
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 1, 1, 1);
+		Gdx.gl.glClearColor(1, 1, 1, (float) 0.5);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
 		if ((y <= 4) && (abs(dy) <= 4)) {
@@ -140,9 +143,15 @@ public class Animation extends ApplicationAdapter {
 			y = 0;
 		}
 
+		shapeRenderer.begin(ShapeRenderer.ShapeType.Filled); //I'm using the Filled ShapeType, but remember you have three of them
+		shapeRenderer.setColor(Color.RED);
+		shapeRenderer.rect(25, h - 100, 500, 50);
+		shapeRenderer.end();
+
 		batch.begin();
 		batch.draw(img, x, y);
 		batch.end();
+
 		stage.act();
 		stage.draw();
 	}
@@ -150,6 +159,7 @@ public class Animation extends ApplicationAdapter {
 	@Override
 	public void dispose () {
 		batch.dispose();
+		shapeRenderer.dispose();
 		img.dispose();
 	}
 
